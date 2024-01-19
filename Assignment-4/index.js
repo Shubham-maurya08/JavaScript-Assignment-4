88fa1f407c205c0a179b4cc4dbfcbca6e7575652
Shubham-maurya08 <146092655+Shubham-maurya08@users.noreply.github.com>
2024-01-19 15:46:12 +0530

    Add files via upload

---
+++ Assignment-4/index.js
@@ -0,0 +1,104 @@
+//Solution:1
+  // let num = prompt("enter the number")
+
+  // if(num <= 10000){
+  //     // Convert the number to a string
+  // let str = num.toString();
+  // // Get the length of the string
+  // let countnumber = str.length;
+  // // Print the count of digits
+  // console.log(countnumber);
+  // }else{
+  //   console.log("enter the number less than 10000");
+  // }
+
+//Solution:2
+
+// let num = prompt("Enter a number");
+// let count = 0;
+
+// if (num <= 10000000) {
+//   for (let i = num; i > 0; i = Math.floor(i / 10)) {
+//     if (i % 10 == 5)
+//     count++; 
+//   }
+//   console.log(count);
+// } else {
+//   console.log("Please enter the number less than 10000000");
+// }
+
+// Solution:3
+// let N = prompt("Enter a number");
+// let sum_even = 0;
+// if (N <= 500) {
+//   for (let i = 1; i <= N; i++) {
+//     if (i % 2 == 0) {
+//       sum_even = sum_even + i;
+//     }
+//   }
+//   console.log(sum_even);
+// } else {
+//   console.log("Please enter no. less than 500");
+// }
+
+//Solution:4
+// let num = prompt("Enter the number");
+// let sum = 0;
+
+// if(num <= 10000){
+//   for(let i = 1; i <= num; i++){
+//     sum = sum + i;
+//   }
+//   console.log(sum);
+// }else{
+//   console.log("Enter the number less than 10000");
+// }
+
+
+// Solution: 5
+// let num = prompt("Enter the number");
+//   if(num <= 100){
+//     for(let i = 2; i <= num; i++){
+//     if(i % 2 != 0){
+//       console.log(i);
+//     }
+//     }
+// }else{
+//   console.log("Enter the number less than 100");
+// }
+
+//Solution:6
+// let a = 5;
+// star = "";
+// for(let i = 1; i <= a; i++){
+//    star += "*" + " ";
+//    console.log(star);
+// }
+
+// Solution:7
+// let num = prompt("Enter the number ");
+
+//     if(num % 2 == 1){
+//         console.log("YES")
+//     }else{
+//         console.log("NO");
+//     }
+
+// Solution:8
+// let num = prompt("Enter the number ");
+
+// for(let i = 1; i <= num; i++){
+//   console.log(i);
+// }
+
+//Solution: 9
+// let a = prompt("Enter a number");
+// const n = 10;
+// for(let i=1; i <= n; i++){
+//     const multiply = a*i;
+//     console.log(a + "*" + i + "="+ multiply);
+// }
+
+
+
+
